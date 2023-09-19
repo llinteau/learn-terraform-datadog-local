861add9d1bd22d24f49e4d955928e501442229c4
llinteau <llinteau12@gmail.com>
2023-09-19 06:11:50 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,4 +3,14 @@ variable "application_name" {
   description = "Application Name"
   default     = "beacon"
 }
+variable "tfc_org" {
+  type        = string
+  description = "LYSETTE_ORG2"
+}
+
+variable "tfc_workspace" {
+  type        = string
+  description = "TFC Workspace"
+  default     = "learn-terraform-eks"
+}
 
