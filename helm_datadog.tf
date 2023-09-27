55567950fadac23acfca5ca72940028db11c1b40
llinteau <llinteau12@gmail.com>
2023-09-27 06:57:22 -0400

    Update helm_datadog.tf

--- helm_datadog.tf
+++ helm_datadog.tf
@@ -1,1 +1,79 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "datadog_agent" {
+  name       = "datadog-agent"
+  chart      = "datadog"
+  repository = "https://helm.datadoghq.com"
+  version    = "3.10.9"
+  namespace  = kubernetes_namespace.beacon.id
+
+  set_sensitive {
+    name  = "datadog.apiKey"
+    value = var.datadog_api_key
+  }
+
+  set {
+    name  = "datadog.site"
+    value = var.datadog_site
+  }
+
+  set {
+    name  = "datadog.logs.enabled"
+    value = true
+  }
+
+  set {
+    name  = "datadog.logs.containerCollectAll"
+    value = true
+  }
+
+  set {
+    name  = "datadog.leaderElection"
+    value = true
+  }
+
+  set {
+    name  = "datadog.collectEvents"
+    value = true
+  }
+
+  set {
+    name  = "clusterAgent.enabled"
+    value = true
+  }
+
+  set {
+    name  = "clusterAgent.metricsProvider.enabled"
+    value = true
+  }
+
+  set {
+    name  = "networkMonitoring.enabled"
+    value = true
+  }
+
+  set {
+    name  = "systemProbe.enableTCPQueueLength"
+    value = true
+  }
+
+  set {
+    name  = "systemProbe.enableOOMKill"
+    value = true
+  }
+
+  set {
+    name  = "securityAgent.runtime.enabled"
+    value = true
+  }
+
+  set {
+    name  = "datadog.hostVolumeMountPropagation"
+    value = "HostToContainer"
+  }
+}
 
