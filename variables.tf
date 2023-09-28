86f6f7eaee0f3b1fecd48d4880565002c6d5e359
llinteau <llinteau12@gmail.com>
2023-09-28 07:31:01 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,3 +16,16 @@ variable "tfc_workspace" {
   default     = "learn-terraform-eks"
 }
 
+variable "datadog_app_key" {
+  type        = string
+  description = "Datadog demo app key"
+  default     = "a76c1fe09502e50d77b668625c223f956b457c29"
+  sensitive   = true
+}
+
+variable "datadog_api_key" {
+  type        = string
+  description = "Datadog demo api key"
+  default     = "fc593c8b14565b9af00479084df18f14"
+  sensitive   = true
+}
