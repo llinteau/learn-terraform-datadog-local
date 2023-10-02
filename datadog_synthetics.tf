2e592f9e854fecf5d25ce45926ca26c75f7d1371
llinteau <llinteau12@gmail.com>
2023-10-02 17:15:28 -0400

    Update datadog_synthetics.tf

--- datadog_synthetics.tf
+++ datadog_synthetics.tf
@@ -1,1 +1,28 @@
+resource "datadog_synthetics_test" "beacon" {
+  type    = "api"
+  subtype = "http"
+
+  request_definition {
+    method = "GET"
+    url    = "http://<Host_URL>"
+  }
+
+  assertion {
+    type     = "statusCode"
+    operator = "is"
+    target   = "200"
+  }
+
+  locations = ["aws:us-west-2"]
+  options_list {
+    tick_every          = 900
+    min_location_failed = 1
+  }
+
+  name    = "Beacon API Check"
+  message = "Oh no! Light from the Beacon app is no longer shining!"
+  tags    = ["app:beacon", "env:demo"]
+
+  status = "live"
+}
 
