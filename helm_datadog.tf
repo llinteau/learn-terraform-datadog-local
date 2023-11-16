d54360984b905dae183e95ff544e9b3c77cf56bb
llinteau <llinteau12@gmail.com>
2023-11-16 14:30:39 -0500

    Added all the settings that we want enabled by default in our environment

--- helm_datadog.tf
+++ helm_datadog.tf
@@ -21,9 +21,19 @@ resource "helm_release" "datadog_agent" {
     value = var.datadog_site
   }
 
+   set {
+    name  = "fips.enabled"
+    value =  true
+  }
+
+  set {
+    name  = "fips.use_https"
+    value =  false
+  }
+
   set {
     name  = "datadog.logs.enabled"
-    value = true
+    value = false
   }
 
   set {
@@ -62,6 +72,21 @@ resource "helm_release" "datadog_agent" {
   }
 
   set {
+    name  = "datadog.processAgent.enabled"
+    value = true
+  }
+
+  set {
+    name  = "datadog.processAgent.processCollection"
+    value = true
+  }
+
+  set {
+    name  = "datadog.dogstatsd.useHostPort"
+    value = true
+  }
+
+  set {
     name  = "systemProbe.enableTCPQueueLength"
     value = true
   }
@@ -85,4 +110,40 @@ resource "helm_release" "datadog_agent" {
     name  = "datadog.serviceMonitoring.enabled"
     value =  true
   }
+
+  set {
+    name  = "datadog.kubeStateMetricsCore.enabled"
+    value =  true
+  }
+
+  set {
+    name  = "datadog.kubeStateMetricsEnabled"
+    value =  false
+  }
+
+  set {
+    name  = "datadog.kubelet.tlsVerify"
+    value =  false
+  }
+
+  set {
+    name  = "clusterAgent.admissionController.enabled"
+    value =  true
+  }
+
+   set {
+    name  = "clusterAgent.admissionController.mutateUnlabelled"
+    value =  false
+  }
+
+   set {
+    name  = "agents.useConfigMap"
+    value =  true
+  }
+
+   set {
+    name  = "agents.customAgentConfig"
+    value =  true
+  }
+
 }
