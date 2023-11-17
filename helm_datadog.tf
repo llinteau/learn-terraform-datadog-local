c99d6d031bb2c67ad2a4228cb10d1c9738ca6fde
llinteau <llinteau12@gmail.com>
2023-11-17 18:45:28 -0500

    Adjusting to add more custom settings I want to play with hopefully this doesn't break

--- helm_datadog.tf
+++ helm_datadog.tf
@@ -8,9 +8,19 @@ resource "helm_release" "datadog_agent" {
   name       = "datadog-agent"
   chart      = "datadog"
   repository = "https://helm.datadoghq.com"
-  version    = "3.10.9"
+  version    = "3.49.0"
   namespace  = kubernetes_namespace.beacon.id
-  
+
+# Use this to reference the custom yaml file with our unique
+# settings to be changed from defaults.
+  values = [
+    file("datadog.values.yml"),
+#    templatefile("${path.root}/templates/datadog.env.yml.tpl", {
+#      region       = local.region
+#      cluster_name = var.cluster_name
+#    })
+  ]
+
   set_sensitive {
     name  = "datadog.apiKey"
     value = var.datadog_api_key
@@ -21,187 +31,39 @@ resource "helm_release" "datadog_agent" {
     value = var.datadog_site
   }
 
-#  FIPS provides encryption. This is mandatory for ATO
-#  set {
-#    name  = "fips.enabled"
-#    value =  true
-#  }
-
-#  FIPS provides encryption. This is mandatory for ATO
-#  set {
-#    name  = "fips.use_https"
-#    value =  false
-#  }
-
-#  Logs are not allowed now but will be considered later. 
+  ## Enable logs agent and provide custom configs
+  # datadog.logs.enabled -- Enables this to activate Datadog Agent log collection
   set {
     name  = "datadog.logs.enabled"
     value = false
   }
 
-#  Logs are not allowed now but will be considered later. Logging for Containers
+  # datadog.logs.containerCollectAll -- Enable this to allow log collection for all containers
+  ## ref: https://docs.datadoghq.com/agent/basic_agent_usage/kubernetes/#log-collection-setup
   set {
     name  = "datadog.logs.containerCollectAll"
-    value = true
+    value = false
   }
 
-#  Baseline Application monitoring
-  set {
-    name  = "datadog.apm.portEnabled"
-    value = true
-  }
-
-#  Enables leader election mechanism for event collection
-  set {
-    name  = "datadog.leaderElection"
-    value = true
-  }
-
-#  Enables this to start event collection from the kubernetes API
-  set {
-    name  = "datadog.collectEvents"
-    value = true
-  }
-
-## This is the Datadog Cluster Agent implementation that handles cluster-wide
-## metrics more cleanly, separates concerns for better rbac, and implements
-## the external metrics API so you can autoscale HPAs based on datadog metrics
-## ref: https://docs.datadoghq.com/containers/cluster_agent/ 
-  set {
-    name  = "clusterAgent.enabled"
-    value = true
-  }
-
-
-# Enable the metricsProvider to be able to scale based on metrics in Datadog
-  set {
-    name  = "clusterAgent.metricsProvider.enabled"
-    value = true
-  }
-
-
-# Enable network performance monitoring
-  set {
-    name  = "datadog.networkMonitoring.enabled"
-    value = true
-  }
-
-
-# Enable live process monitoring agent
-  set {
-    name  = "datadog.processAgent.enabled"
-    value = true
-  }
-
-## Note: /etc/passwd is automatically mounted when `processCollection` or `processDiscovery` is enabled.
-## ref: https://docs.datadoghq.com/graphing/infrastructure/process/#kubernetes-daemonset
-#  Enable process collection in process monitoring agent
-  set {
-    name  = "datadog.processAgent.processCollection"
-    value = true
-  }
-
-
-## ref: https://docs.datadoghq.com/agent/kubernetes/dogstatsd/
-## To emit custom metrics from your Kubernetes application, use DogStatsD.
-  set {
-    name  = "datadog.dogstatsd.port"
-    value = "8125"
-  }
-
-# Sets the hostPort to the same value of the container port
+  ## ref: https://docs.datadoghq.com/agent/kubernetes/dogstatsd/
+  # To emit custom metrics from your Kubernetes application, use DogStatsD.
+  # datadog.dogstatsd.port -- Override the Agent DogStatsD port
+  # datadog.dogstatsd.useHostPort -- Sets the hostPort to the same value of the container port
   set {
     name  = "datadog.dogstatsd.useHostPort"
     value = true
   }
 
-
-  set {
-    name  = "datadog.dogstatsd.nonLocalTraffic"
-    value = "true"
-  }
-
-# Enable the TCP queue length eBPF-based check
+  # datadog.systemProbe.enableTCPQueueLength -- Enable the TCP queue length eBPF-based check
   set {
     name  = "systemProbe.enableTCPQueueLength"
     value = true
   }
 
-# Enable the OOM kill eBPF-based check
+  ## Enable systemProbe agent and provide custom configs
+  # datadog.systemProbe.enableOOMKill -- Enable the OOM kill eBPF-based check
   set {
     name  = "systemProbe.enableOOMKill"
     value = true
-  }
-
-# Set to true to enable Cloud Workload Security (CWS)
-  set {
-    name  = "securityAgent.runtime.enabled"
-    value = true
-  }
-
-
-# Allow to specify the `mountPropagation` value on all volumeMounts using HostPath
-## ref: https://kubernetes.io/docs/concepts/storage/volumes/#mount-propagation
-  set {
-    name  = "datadog.hostVolumeMountPropagation"
-    value = "HostToContainer"
-  }
-
-# Enable Universal Service Monitoring
-  set {
-    name  = "datadog.serviceMonitoring.enabled"
-    value =  true
-  }
-
-
-# Enable the kubernetes_state_core check in the Cluster Agent (Requires Cluster Agent 1.12.0+)
-  set {
-    name  = "datadog.kubeStateMetricsCore.enabled"
-    value =  true
-  }
-
-
-# deploys the kube-state-metrics deployment
-## ref: https://github.com/kubernetes/kube-state-metrics/tree/kube-state-metrics-helm-chart-2.13.2/charts/kube-state-metrics
-# The kubeStateMetricsEnabled option will be removed in the 4.0 version of the Datadog Agent chart.
-  set {
-    name  = "datadog.kubeStateMetricsEnabled"
-    value =  false
-  }
-
-# Toggle kubelet TLS verification
-  set {
-    name  = "datadog.kubelet.tlsVerify"
-    value =  false
-  }
-
-# Enable the admissionController to be able to inject APM/Dogstatsd 
-# config and standard tags (env, service, version) automatically into your pods
-  set {
-    name  = "clusterAgent.admissionController.enabled"
-    value =  true
-  }
-
-# Enable injecting config without having the pod label 'admission.datadoghq.com/enabled="true"'
-  set {
-    name  = "clusterAgent.admissionController.mutateUnlabelled"
-    value =  false
-  }
-
-# Configures a configmap to provide the agent configuration. Use this in combination with 
-# the `agents.customAgentConfig` parameter.
-  set {
-    name  = "agents.useConfigMap"
-    value =  true
-  }
-
-# Specify custom contents for the datadog agent config (datadog.yaml)
-  ## ref: https://docs.datadoghq.com/agent/guide/agent-configuration-files/?tab=agentv6
-  ## ref: https://github.com/DataDog/datadog-agent/blob/main/pkg/config/config_template.yaml
-  ## Note the `agents.useConfigMap` needs to be set to `true` for this parameter to be taken into account.
-#  https://github.com/DataDog/helm-charts/blob/main/charts/datadog/values.yaml#L1720
-  set {
-    name  = "agents.customAgentConfig"
-    value =  true
   }
 }
