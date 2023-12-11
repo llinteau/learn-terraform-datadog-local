e57e254b3ffd358e5dc4bd356f4f3f9aeaea23e6
llinteau <llinteau12@gmail.com>
2023-12-11 18:56:03 -0500

    trying tagging via tf

--- datadog_integration_aws_tag_filter.tf
+++ datadog_integration_aws_tag_filter.tf
@@ -1,4 +1,5 @@
-# Create a new Datadog - Amazon Web Services integration tag filter
+# https://registry.terraform.io/providers/DataDog/datadog/latest/docs/resources/integration_aws_tag_filter
+# Provides a Datadog AWS tag filter resource. This can be used to create and manage Datadog AWS tag filters.
 resource "datadog_integration_aws_tag_filter" "foo" {
   account_id     = "746776318030"
   namespace      = "sqs"
