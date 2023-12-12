01f7ea90eae18c466e55265ccd9aca9eca4de2d0
llinteau <llinteau12@gmail.com>
2023-12-11 19:07:57 -0500

    namechange

--- datadog_integration_aws_tag_filter.tf
+++ datadog_integration_aws_tag_filter.tf
@@ -3,5 +3,5 @@
 resource "datadog_integration_aws_tag_filter" "foo" {
   account_id     = "746776318030"
   namespace      = "sqs"
-  tag_filter_str = "business_application:roller_skate_state"
+  tag_filter_str = "added_via_tf_application:rollerskate_haven"
 }
