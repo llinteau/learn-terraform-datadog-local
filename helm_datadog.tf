3a5976b0da3286413b6eb4ecfed505cad5551ce6
llinteau <llinteau12@gmail.com>
2024-02-19 16:02:55 -0500

    s

--- helm_datadog.tf
+++ helm_datadog.tf
@@ -8,7 +8,7 @@ resource "helm_release" "datadog_agent" {
   name       = "datadog-agent"
   chart      = "datadog"
   repository = "https://helm.datadoghq.com"
-  version    = "3.49.7"
+  version    = "3.56.0"
   namespace  = kubernetes_namespace.beacon.id
 
 # Use this to reference the custom yaml file with our unique
